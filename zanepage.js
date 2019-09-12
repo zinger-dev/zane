e8dfa7dcdc906bfbc65ffd9a4000711a23a71e26
zinger-dev <55172640+zinger-dev@users.noreply.github.com>
2019-09-12 07:27:53 -0700

    Add files via upload

---
+++ zanepage.js
@@ -0,0 +1,7 @@
+$("#swappic").click(function() {
+  $("#tiger").attr(
+    "src",
+    "https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/an-angry-tiger-roars-fiercely-derrick-neill.jpg"
+  );
+  $("#tgrbutttxt").text("I warned you...");
+});
